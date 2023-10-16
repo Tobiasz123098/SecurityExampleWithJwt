ab034a92d0e20012a071647534299d45327a622a
Tobiasz Czaprowicz <tczaprowicz@gmail.com>
2023-10-16 15:09:16 +0200

    Initialization commit

---
+++ src/main/java/com/javaguides/securityExampleJwt/SecurityExampleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.securityExampleJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExampleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExampleJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaguides/securityExampleJwt/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.javaguides.securityExampleJwt.repository;
+
+import com.javaguides.securityExampleJwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+}
